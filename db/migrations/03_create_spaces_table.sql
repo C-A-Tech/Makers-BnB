44ca1d2b4c1f6cda6511ade262e107e5cf908d21
Abdur Rehman <wabdulrehman@yahoo.com>
2021-04-27 17:28:07 +0100

    changed command for spaces table

--- db/migrations/03_create_spaces_table.sql
+++ db/migrations/03_create_spaces_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE spaces (
   id SERIAL PRIMARY KEY,
   name VARCHAR (60) NOT NULL,
   description VARCHAR (500) UNIQUE NOT NULL,
-  price INTEGER (4) NOT NULL,
+  price INT NOT NULL,
   availability BOOLEAN
-  user_id INT REFERENCES users(id)
 );
+ALTER TABLE spaces ADD COLUMN user_id INT REFERENCES users(id);
