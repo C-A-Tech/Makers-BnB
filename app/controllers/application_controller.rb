627c1f03a48490bdb5d24dc36d7d753d55ec7e25
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-04-29 13:07:06 +0100

    CSS linkage fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class BDE < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
     enable :sessions
-    set :public_folder, proc { File.join(root, '../public') }
+    set :root, File.dirname(File.expand_path('..', __FILE__))
+    set :public_folder, Proc.new { File.join(root, '../public') }
   end
 end
